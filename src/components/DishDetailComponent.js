ec6d071d1525dfc41d8a5c9093b0c4d94cc92467
L3thal <karthikramakrishnan14@gmail.com>
2020-10-29 21:55:59 +0530

    Fetch Post Comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -32,7 +32,7 @@ const minLength= (len) => (val) => (val) && (val.length >= len);
     }
     handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
   }
   toggleModal() {
     this.setState({
@@ -109,7 +109,7 @@ const minLength= (len) => (val) => (val) && (val.length >= len);
   }
   }
 
-  function RenderComments({comments, addComment, dishId}) {
+  function RenderComments({comments, postComment, dishId}) {
     if (comments == null || comments.length === 0) {
       return (
         <div></div>
@@ -131,7 +131,7 @@ const minLength= (len) => (val) => (val) && (val.length >= len);
         <ul className="list-unstyled">
           { renderedComments }
         </ul>
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
 
       </div>
     );
@@ -176,7 +176,7 @@ const minLength= (len) => (val) => (val) && (val.length >= len);
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments}
-                          addComment ={props.addComment}
+                          postComment ={props.postComment}
                           dishId={props.dish.id}
                         />
                     </div>
